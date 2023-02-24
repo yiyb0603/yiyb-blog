1872284fa324899bbe3faf2c26858ab6a3b56f44
권용빈 <yiyb0603@gmail.com>
2023-02-24 23:57:41 +0900

    Chore: 로고 이미지 추가

---
+++ src/assets/images.ts
@@ -0,0 +1,7 @@
+import generateStaticPath from '@/utils/cdn/generateStaticPath';
+
+export const images = {
+  logo: {
+    MAIN_TEXT_IMAGE: generateStaticPath('/images/logo/main_text_logo.webp'),
+  },
+};
