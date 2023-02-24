2da4007e6e35c0156209dbeb319fbd6a71f7ba65
권용빈 <yiyb0603@gmail.com>
2023-02-24 21:46:44 +0900

    Chore: 폰트 preload 설정 및 기타 스타일링 유틸 추가

---
+++ src/styles/device.ts
@@ -0,0 +1,35 @@
+export const deviceSizes = {
+  smallMobile: '350px',
+  mobile: '400px',
+  mediumMobile: '500px',
+  smallTablet: '650px',
+  tablet: '768px',
+  mediumTablet: '850px',
+  smallLaptop: '950px',
+  laptop: '1024px',
+  mediumLaptop: '1200px',
+} as const;
+
+type DeviceSizes = keyof typeof deviceSizes;
+
+type Device = Record<DeviceSizes, `@media only screen and (max-width: ${typeof deviceSizes[DeviceSizes]})`>;
+
+export const device: Device = Object.entries(deviceSizes).reduce((device, [deviceName, deviceSize]) => {
+  device[deviceName as DeviceSizes] = `@media only screen and (max-width: ${deviceSize})`;
+
+  return device;
+}, {} as Device);
+
+export const constantSafeArea = {
+  top: 'constant(safe-area-inset-top)',
+  left: 'constant(safe-area-inset-left)',
+  right: 'constant(safe-area-inset-right)',
+  bottom: 'constant(safe-area-inset-bottom)',
+} as const;
+
+export const envSafeArea = {
+  top: 'env(safe-area-inset-top)',
+  left: 'env(safe-area-inset-left)',
+  right: 'env(safe-area-inset-right)',
+  bottom: 'env(safe-area-inset-bottom)',
+} as const;
---
+++ src/utils/cdn/generateStaticPath.ts
@@ -0,0 +1,12 @@
+import dotenv from '@/libs/dotenv';
+import isEmpty from '../is-packages/isEmpty';
+
+const generateStaticPath = (path: string): string => {
+  if (dotenv.DEV_MODE || isEmpty(dotenv.CDN_SERVER)) {
+    return path;
+  }
+
+  return `${dotenv.CDN_SERVER}${path}`;
+}
+
+export default generateStaticPath;
