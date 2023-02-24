077ad4dbfc0798ff551be42b046f904768182170
권용빈 <yiyb0603@gmail.com>
2023-02-24 23:57:25 +0900

    Fix: redux wrapper를 사용하여 App에 주입하는 방식 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
+import { Provider as ReduxProvider } from 'react-redux';
 import { NextComponentType } from 'next';
 import { AppContext, AppInitialProps, AppProps } from 'next/app';
 import { wrapper } from '@/stores/nextStore';
 import GlobalStyle from '@/styles/GlobalStyle';
 import StyleProvider from '@/components/Providers/StyleProvider';
+import UserTemplate from '@/components/Templates/UserTemplate';
 
 type MyAppProps = AppProps & {};
 
@@ -11,16 +13,27 @@ type CustomAppComponent = NextComponentType<AppContext, AppInitialProps, MyAppPr
 const MyApp: CustomAppComponent = ({
   Component,
   pageProps,
+  ...reduxProps
 }) => {
-  return (
-    <>
-      <StyleProvider>
-        <Component {...pageProps} />
-      </StyleProvider>
+  const {
+    store,
+  } = wrapper.useWrappedStore(reduxProps)
 
-      <GlobalStyle />
-    </>
+  return (
+    <ReduxProvider
+      store={store}
+    >
+      <StyleProvider>
+        <UserTemplate>
+          <Component
+            {...pageProps}
+          />
+        </UserTemplate>
+
+        <GlobalStyle />
+      </StyleProvider>
+    </ReduxProvider>
   );
 }
 
-export default wrapper.withRedux(MyApp);
+export default MyApp;
