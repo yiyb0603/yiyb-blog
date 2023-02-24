77df77a1f8a832ae23667ccd0c7050a9b7c1b2bb
권용빈 <yiyb0603@gmail.com>
2023-02-24 20:07:01 +0900

    Chore: contentlayer config 설정

---
+++ contentlayer.config.ts
@@ -0,0 +1,38 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  contentType: 'mdx',
+  filePathPattern: `**/*.mdx`,
+  fields: {
+    title: {
+      type: 'string',
+      required: true,
+    },
+    description: {
+      type: 'string',
+      required: true,
+    },
+    category: {
+      type: 'string',
+      required: true,
+    },
+    createdAt: {
+      type: 'date',
+      required: true,
+    },
+    updatedAt: {
+      type: 'date',
+      required: false,
+    },
+  },
+}));
+
+const contentSource = makeSource({
+  contentDirPath: 'posts',
+  documentTypes: [
+    Post,
+  ],
+});
+
+export default contentSource;
