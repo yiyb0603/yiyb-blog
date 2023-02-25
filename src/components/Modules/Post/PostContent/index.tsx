2299763f4b54278c05e2a2ac5511991a13f63d34
권용빈 <yiyb0603@gmail.com>
2023-02-25 18:58:33 +0900

    feature: 마크다운 기본 스타일링 코드 추가

--- src/components/Modules/Post/PostContent/index.tsx
+++ src/components/Modules/Post/PostContent/index.tsx
@@ -1,4 +1,5 @@
 import { useMDXComponent } from 'next-contentlayer/hooks';
+import styled from 'styled-components';
 
 type PostContentProps = {
   code: string;
@@ -10,10 +11,59 @@ const PostContent = ({
   const MDXComponent = useMDXComponent(code);
 
   return (
-    <div>
+    <PostContentContainer>
       <MDXComponent />
-    </div>
-  )
+    </PostContentContainer>
+  );
 }
+
+const PostContentContainer = styled.div`
+  img {
+    width: 100%;
+    margin: 1rem 0;
+  }
+
+  pre {
+    width: 100%;
+    margin: 1rem 0;
+    word-break: break-all;
+    overflow: auto;
+    padding: 1.5rem;
+    border-radius: 5px;
+    background-color: ${({ theme }) => theme.color.black};
+  }
+
+  h1 {
+    margin: 1rem 0;
+    font-size: ${({ theme }) => theme.fontSize.LARGE};
+  }
+
+  h2,
+  h3,
+  h4 {
+    margin: 1rem 0;
+    font-size: ${({ theme }) => theme.fontSize.EXTRA_BIG};
+  }
+
+  ol {
+    margin: 1rem 0;
+  }
+
+  p,
+  li,
+  span {
+    white-space: pre-wrap;
+    word-break: break-all;
+    line-height: 1.6;
+    font-size: ${({ theme }) => theme.fontSize.NORMAL};
+  }
+
+  blockquote {
+    margin: 1rem 0;
+    padding: 1.5rem;
+    border-radius: 5px;
+    background-color: ${({ theme }) => theme.color.background3};
+  }
+`;
 
 export default PostContent;
