55f352cd5e37d3f7062ea9ab0fa6ec428441ea28
권용빈 <yiyb0603@gmail.com>
2023-02-26 14:40:21 +0900

    feature: 검색엔진 수집용 사이트맵 페이지 추가

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,44 @@
+import { NextPage, GetServerSideProps } from 'next';
+import dayjs from 'dayjs';
+import { allPosts } from '@/contentlayer/generated';
+import { pageRoute } from '@/libs/models/route';
+import generateFullURL from '@/utils/string/generateFullURL';
+
+const SitemapPage: NextPage = () => {
+  return null;
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { res } = ctx;
+
+  let pagesXMLString = '';
+
+  for (const {
+    _raw,
+    createdAt,
+  } of allPosts) {
+    pagesXMLString += `
+      <url>
+        <loc>${generateFullURL(`${pageRoute.POSTS}/${encodeURIComponent(_raw.flattenedPath)}`)}</loc>
+        <lastmod>${dayjs(createdAt).format('YYYY-MM-DDTHH:mm:ssZ')}</lastmod>
+      </url>
+    `;
+  }
+
+  const xmlContents = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+      ${pagesXMLString}
+    </urlset>
+  `;
+
+  res?.setHeader('Content-Type', 'text/xml');
+
+  res?.write(xmlContents);
+  res?.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SitemapPage;
