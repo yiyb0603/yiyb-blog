e06d91a31775ef7350a40225b6ed68ff4f09f8f7
권용빈 <yiyb0603@gmail.com>
2023-03-02 21:39:51 +0900

    feature: 구글, 네이버, Bing 서치어드바이저 추가 및 GA 연동

--- src/libs/dotenv.ts
+++ src/libs/dotenv.ts
@@ -1,7 +1,16 @@
 const dotenv = {
   ASSET_PREFIX: process.env.NEXT_PUBLIC_ASSET_PREFIX as string,
   CDN_SERVER: process.env.NEXT_PUBLIC_CDN_SERVER as string,
+
+  GOOGLE_ANALYTICS_ID: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID as string,
+  GOOGLE_SITE_VERIFICATION_ID: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION_ID as string,
+
+  NAVER_SITE_VERIFICATION_ID: process.env.NEXT_PUBLIC_NAVER_SITE_VERIFICATION_ID as string,
+
+  BING_SITE_VERIFICATION_ID: process.env.NEXT_PUBLIC_BING_SITE_VERIFICATION_ID as string,
+
   APP_URL: process.env.NEXT_PUBLIC_URL as string,
+
   DEV_MODE: process.env.NODE_ENV === 'development',
 } as const;
 
