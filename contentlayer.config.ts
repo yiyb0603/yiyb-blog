b4136f0251e5353d732c0f44636aaa76c114e5eb
권용빈 <yiyb0603@gmail.com>
2024-01-17 11:45:41 +0900

    Refactor: posts의 단일 MDX 파일들을 카테고리명/각 파일명을 갖는 폴더명으로 각각 이동

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -19,10 +19,6 @@ export const Post = defineDocumentType(() => ({
       type: 'string',
       required: true,
     },
-    category: {
-      type: 'string',
-      required: true,
-    },
     thumbnail: {
       type: 'string',
       required: true,
@@ -34,6 +30,25 @@ export const Post = defineDocumentType(() => ({
     updatedAt: {
       type: 'date',
       required: false,
+    },
+  },
+  computedFields: {
+    url: {
+      type: 'string',
+      resolve: (post) => {
+        const [_, path] = post._raw.flattenedPath.split('/');
+
+        return `/posts/${path}`;
+      },
+    },
+
+    category: {
+      type: 'string',
+      resolve: (post) => {
+        const [category] = post._raw.flattenedPath.split('/');
+
+        return category;
+      },
     },
   },
 }));
